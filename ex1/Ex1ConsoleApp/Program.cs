55a2ac851ed6b33d9a9df505f35e904054a761e4
rbutalia <randeep.butalia@gmail.com>
2017-11-12 13:35:35 -0800

    adding additional projects for testing the app

---
+++ ex1/Ex1ConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using ef1.Data;
+using ex1.Domain;
+using System;
+
+namespace Ex1ConsoleApp
+{
+    class Program
+    {
+        private static Ex1DataContext _context = new Ex1DataContext();
+        static void Main(string[] args)
+        {
+            InsertCompany();
+            Console.WriteLine("Hello World!");
+        }
+
+
+        private static void InsertCompany()
+        {
+            var ex1 = new Company { CompanyName = "Manit Company", SalesTax = 0.13m, TextIdentifier = "CA", CreatedBy = "Randeep B", CreatedDate = DateTime.UtcNow };
+            _context.Companies.Add(ex1);
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ ex1/Ex1WebApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Ex1WebApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    var env = hostingContext.HostingEnvironment;
+                    config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                          .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+                    config.AddEnvironmentVariables();
+                })
+                .ConfigureLogging((hostingContext, logging) =>
+                {
+                     logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                    //logging.AddConfiguration("appsettings.json", optional: true, reloadOnChange: true)
+                     //      .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+                     logging.AddConsole();
+                    //logging.AddDebug();
+                })
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .UseApplicationInsights()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
